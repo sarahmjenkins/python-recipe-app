15fcfbef811affb02e28cd1654723f34bce1d2b0
sarahmjenkins <sarahmjenkins88@gmail.com>
2023-05-02 12:08:10 -0600

    updated create_recipe and Recipes table

--- exercise_1.6/recipe_mysql.py
+++ exercise_1.6/recipe_mysql.py
@@ -1,27 +1,7 @@
-import mysql.connector
-
-conn = mysql.connector.connect(
-  host='localhost',
-  user='cf-python',
-  passwd='password'
-)
-cursor = conn.cursor()
-
-cursor.execute('CREATE DATABASE IF NOT EXISTS task_database')
-cursor.execute('USE task_database')
-
-cursor.execute('''CREATE TABLE IF NOT EXISTS Recipes(
-  id INT PRIMARY KEY,
-  name VARCHAR(50),
-  ingredients VARCHAR(255),
-  cooking_time INT,
-  difficulty VARCHAR(20)
-)''')
-
 # Main Menu
 def main_menu(conn, cursor):
   choice = ''
-  
+
   while(choice != 'quit'):
     print('-----------------------------')
     print('Main Menu')
@@ -32,7 +12,7 @@ def main_menu(conn, cursor):
     print('3. Update an existing recipe.')
     print('4. Delete a recipe.')
     print('Type "quit" to exit the program.')
-    choice = input('Enter your choie here: ')
+    choice = input('Enter your choice here: ')
 
     if choice == '1':
       create_recipe(conn, cursor)
@@ -47,13 +27,13 @@ def main_menu(conn, cursor):
 # Option One: create recipe
 def create_recipe(conn, cursor):
   def calc_difficulty(cooking_time, ingredients_list):
-    if cooking_time < 10 and ingredients_list < 4:
+    if cooking_time < 10 and len(ingredients_list) < 4:
       difficulty = 'Easy'
-    elif cooking_time < 10 and ingredients_list >= 4:
+    elif cooking_time < 10 and len(ingredients_list) >= 4:
       difficulty = 'Medium'
-    elif cooking_time >= 10 and ingredients_list < 4:
+    elif cooking_time >= 10 and len(ingredients_list) < 4:
       difficulty = 'Intermediate'
-    elif cooking_time >= 10 and ingredients_list >= 4:
+    elif cooking_time >= 10 and len(ingredients_list) >= 4:
       difficulty = 'Hard'
     return difficulty
   
@@ -63,18 +43,16 @@ def create_recipe(conn, cursor):
   name = input('Enter the name of your recipe: ')
   ingredients_string = input('Enter a list of ingredients, separated by commas: ')
   ingredients_list = list(ingredients_string.lower().split(', '))
-  cooking_time = input('Enter how long your recipe takes to make in minutes: ')
+  cooking_time = int(input('Enter how long your recipe takes to make in minutes: '))
   difficulty = calc_difficulty(cooking_time, ingredients_list)
+  print("Difficulty:", difficulty)
 
-  sql = 'INSERT INTO Recipes(name, ingredients, cooking_time, difficulty) VALUES (%s, %s, %s, %s)'
+  sql = 'INSERT INTO Recipes (name, ingredients, cooking_time, difficulty) VALUES (%s, %s, %s, %s)'
   val = (name, ingredients_string, cooking_time, difficulty)
 
   cursor.execute(sql, val)
-
   conn.commit()
   print('Your recipe is saved to the database.')
-
-
 
 # Option Two: search for ingredient
 def search_recipe(conn, cursor):
@@ -92,4 +70,25 @@ def delete_recipe(conn, cursor):
 def quit_program(conn, cursor):
   return print('done')
 
-# Main menu
+# Running the script
+import mysql.connector
+
+conn = mysql.connector.connect(
+  host='localhost',
+  user='cf-python',
+  passwd='password'
+)
+cursor = conn.cursor()
+
+cursor.execute('CREATE DATABASE IF NOT EXISTS task_database')
+cursor.execute('USE task_database')
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS Recipes(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(50),
+  ingredients VARCHAR(255),
+  cooking_time INT,
+  difficulty VARCHAR(20)
+)''')
+
+main_menu(conn, cursor)
