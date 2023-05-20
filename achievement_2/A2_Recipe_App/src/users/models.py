2ba2302f1b15a56bb1315ddbbc183e6e4f283c64
sarahmjenkins <sarahmjenkins88@gmail.com>
2023-05-19 18:36:06 -0600

    finished exercise 2.3

--- achievement_2/A2_Recipe_App/src/users/models.py
+++ achievement_2/A2_Recipe_App/src/users/models.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class User(models.Model):
-    username = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=12)
     password = models.CharField(max_length=12)
-    bio = models.TextField(default='no bio')
+    bio = models.TextField(max_length=250, default='no bio')
 
     def __str__(self):
-        return f'Profile of {self.user.username}'
+        return f'Profile of {self.username}'
